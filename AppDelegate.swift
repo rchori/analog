b1a23d73c0b47715f2cc2613bffb68d3d98d81ea
rchori <65335676+rchori@users.noreply.github.com>
2020-05-14 12:27:49 +0300

    Create AppDelegate.swift

---
+++ AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  AnalogV1
+//
+//  Created by Vlad on 29.01.2020.
+//  Copyright © 2020 Vlad. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
